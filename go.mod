b7bb9b6a1c76b8569b4f824ebee1363ae83ae6f4
Way2School4Cool <calebem1998@gmail.com>
2025-07-12 23:11:32 -0400

    fix menu and add some notes

---
+++ go.mod
@@ -0,0 +1,3 @@
+module ImageManipulation
+
+go 1.22.2
--- main.go
+++ main.go
@@ -16,79 +16,68 @@ import (
 // arg 2 == File Input name
 // arg 3 == File Output name
 func main() {
-    start := time.Now()
-    var checkpoint time.Time
-    //var shiftedImage image.Image
+	start := time.Now()
+	var checkpoint time.Time
+	//var shiftedImage image.Image
 
-    option := os.Args[1]
-    inputFilename := os.Args[2]
-    outputFilename := os.Args[3]
+	option := os.Args[1]
+	inputFilename := os.Args[2]
+	outputFilename := os.Args[3]
+	var outputImage image.Image
 
-    if (inputFilename == "" || outputFilename == "") {
-        log.Fatal("No filenames given")
-    }
+	if inputFilename == "" || outputFilename == "" {
+		log.Fatal("No filenames given")
+	}
 
-    if option == "l" {
-        luminanceImage := manip.LuminanceImage(inputFilename)
+	switch option {
+	case "l": // Generate Black and White Image based on luminance
+		outputImage = manip.LuminanceImage(inputFilename)
 
-        checkpoint = time.Now()
-        fmt.Printf("Generated image in: %v\n", checkpoint.Sub(start))
+	case "s": // Pixel Sorting (Based on luminance)
+		luminanceImage := manip.LuminanceImage(inputFilename)
 
-        SaveToPDFFile(luminanceImage, outputFilename)
+		checkpoint = time.Now()
+		fmt.Printf("Generated luminance in: %v\n", checkpoint.Sub(start))
 
-        checkpoint = time.Now()
-        fmt.Printf("Saved to file in: %v\n", checkpoint.Sub(start))
+		outputImage = manip.PixelSort(inputFilename, luminanceImage)
 
-    } else if option == "s" {
-        luminanceImage := manip.LuminanceImage(inputFilename)
+	default: //Perform Shift based on color option
+		outputImage = manip.PerformShift(inputFilename, option)
+	}
 
-        checkpoint = time.Now()
-        fmt.Printf("Generated luminance in: %v\n", checkpoint.Sub(start))
+	checkpoint = time.Now()
+	fmt.Printf("Generated image in: %v\n", checkpoint.Sub(start))
 
-        sortedImage := manip.PixelSort(inputFilename, luminanceImage)
+	SaveToPDFFile(outputImage, outputFilename)
 
-        checkpoint = time.Now()
-        fmt.Printf("Generated image in: %v\n", checkpoint.Sub(start))
-
-        SaveToPDFFile(sortedImage, outputFilename)
-
-        checkpoint = time.Now()
-        fmt.Printf("Saved to file in: %v\n", checkpoint.Sub(start))
-
-    } else {
-
-        shiftedImage := manip.PerformShift(inputFilename, option)
-
-        checkpoint = time.Now()
-        fmt.Printf("Generated image in: %v\n", checkpoint.Sub(start))
-
-        SaveToPDFFile(shiftedImage, outputFilename)
-
-        checkpoint = time.Now()
-        fmt.Printf("Saved to file in: %v\n", checkpoint.Sub(start))
-    }
+	checkpoint = time.Now()
+	fmt.Printf("Saved to file in: %v\n", checkpoint.Sub(start))
 }
 
 func HelpMenu() {
-    menu := []string{
-        "",
-        "Menu:",
-        "g - Greyscale (returns a txt of the greyscaled image)",
-        "",
-    }
-    log.Fatal(strings.Join(menu, "\n"))
+	menu := []string{
+		"",
+		"Menu:",
+		"g - Greyscale (returns a greyscaled image based on luminance)",
+		"s - Pixel Sorting (returns a pixel sorted image based on luminance)",
+		"r - Red Shift (returns a red shifted image)",
+		"b - Blue Shift (returns a blue shifted image)",
+		"a - Alpha Shift (returns a alpha shifted image)",
+		"",
+	}
+	log.Fatal(strings.Join(menu, "\n"))
 }
 
 func SaveToPDFFile(image image.Image, outputFilename string) {
-    outputFile, err := os.Create(outputFilename)
-    if err != nil {
-        log.Fatalf("failed to create: %s\n%s\n", outputFilename, err)
-    }
+	outputFile, err := os.Create(outputFilename)
+	if err != nil {
+		log.Fatalf("failed to create: %s\n%s\n", outputFilename, err)
+	}
 
-    defer outputFile.Close()
+	defer outputFile.Close()
 
-    err = png.Encode(outputFile, image)
-    if err != nil {
-        log.Fatalf("failed to encode: %s\n%s\n", outputFilename, err)
-    }
+	err = png.Encode(outputFile, image)
+	if err != nil {
+		log.Fatalf("failed to encode: %s\n%s\n", outputFilename, err)
+	}
 }
